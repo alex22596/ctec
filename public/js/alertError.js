ea8d6b16a6beab38e92d2766f4d558179e40a563
alex22596 <alex22596@gmail.com>
2017-06-10 21:14:18 -0600

    Más Cambios Varios123.

---
+++ public/js/alertError.js
@@ -0,0 +1,6 @@
+swal({
+    title: "Datos Ingresados Incorrectos",
+    text: "Asegurese de Escribir Correctamente su Nombre de Usuario y Contraseña",
+    type: "error",
+    closeOnConfirm: true
+});
